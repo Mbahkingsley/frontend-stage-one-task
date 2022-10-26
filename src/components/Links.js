297d73b6d857621b6a13739d13765bc2cf03e17c
mbah kingsley <mbahkingsley9@gmail.com>
2022-10-26 15:13:27 +0100

    updated links details and Profile component with twitter username

--- src/components/Links.js
+++ src/components/Links.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css'
-const Links = ({link}) => {
+const Links = () => {
     return (
-        <div className='d-flex align-items-center justify-content-center w-100 bg-primary h-50'>
-            <Button variant='secondary' className='mt-3 w-75'><a href="https://training.zuri.team/">{link}</a></Button>
+        <div className='d-flex flex-column align-items-center justify-content-center w-100 h-50'>
+            <Button variant='secondary' className='mt-3 w-75'><a className='text-decoration-none' href="https://twitter.com/Mbahkingsley13">Twitter Link</a></Button>
+            <Button variant='secondary' className='mt-3 w-75'><a className='text-decoration-none' href="https://training.zuri.team">Zuri Team</a></Button>
+            <Button variant='secondary' className='mt-3 w-75'><a className='text-decoration-none' href="https://books.zuri.team">Zuri Books</a></Button>
+            <Button variant='secondary' className='mt-3 w-75'><a className='text-decoration-none' href="https://books.zuri.team/python-for-beginners?ref_id=<yourslackname>">Python Books</a></Button>
+            <Button variant='secondary' className='mt-3 w-75'><a className='text-decoration-none' href="https://background.zuri.team">Background Checks for Coders</a></Button>
+            <Button variant='secondary' className='mt-3 w-75'><a className='text-decoration-none' href="https://books.zuri.team/design-rules">Design Books</a></Button>
         
         </div>
     )
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -3,8 +3,10 @@ import img from '../wafbec.jpg'
 
 const Profile = () => {
     return (
-        <div className='d-flex align-items-center justify-content-center mt-4 mb-3'>
-            <img className='img-fluid max-width:100; height: auto; w-20 h-10 rounded-circle' src={img} alt="" />
+        <div className='text-center'>
+            <img id='profile_img' className='img-fluid max-width:100; height: auto; w-20 h-10 rounded-circle ml-auto mr-auto mt-5' src={img} alt="" />
+            <p id='twitter'>Nonxxo</p>
+            <p id='slack' className='d-none'>slack username here</p>
         </div>
     )
 };
